d59bda7840abec6e8e5a2859c7e1feba3b726769
Omar Sosa <ososa@psa.com.ar>
2023-03-22 17:53:39 -0300

    Se ordena html

---
+++ html/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE >
+<lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div><a href="./new-type/form_with_name.html"/>: Formulario</a></div>
+    <div><a href="./new-type/new_types.html"/>: Types</a></div>
+    
+    <div><a href="./canva/canva_basico.html"/>: Canva basico</a></div>
+    <div><a href="./canva/canva_circulo.html"/>: Canva circulo</a></div>
+    <div><a href="./canva/canva_degradado_circular.html"/>: Canva Degradado</a></div>
+    <div><a href="./canva/canva_gradiente.html"/>: Canva degradado</a></div>
+    <div><a href="./canva/canva_linea.html"/>: Canva linea</a></div>
+    <div><a href="./canva/canva_texto.html"/>: Canva texto</a></div>
+
+    <div><a href="./animacion/animacion_avance_cuadro.html"/>: Animacion avance cuadro</a></div>
+    <div><a href="./animacion/animacion_ciclo.html"/>: Animacion ciclo</a></div>
+    <div><a href="./animacion/animacion_elevator.html"/>: Animacion elevador</a></div>
+    <div><a href="./animacion/animacion_gradiente.html"/>: Animacion gradiente</a></div>
+    <div><a href="./animacion/clases.html"/>: Animacion clase</a></div>
+    <div><a href="./animacion/iframe.html"/>: Animacion iframe</a></div>
+   
+    <div><a href="./selector/adyacente.html"/>CSS: Selector adyacente </a></div>
+    <div><a href="./selector/hermano.html"/>CSS: Selector hermano</a></div>
+    <div><a href="./selector/hijo.html"/>CSS: Selector hijo</a></div>
+
+    <div><a href="./pseudoclase/first-child.html"/>CSS: Pseudoclase </a></div>
+    <div><a href="./pseudoclase/focus.html"/>CSS: Pseudoclase </a></div>
+    <div><a href="./pseudoclase/focus-link.html"/>CSS: Pseudoclase </a></div>
+    <div><a href="./pseudoclase/last-child.html"/>CSS: Pseudoclase </a></div>
+    <div><a href="./pseudoclase/nth-child.html"/>CSS: Pseudoclase </a></div>
+    <div><a href="./pseudoclase/nth-child-word.html"/>CSS: Pseudoclase </a></div>
+
+    <div><a href="./pseudo-elemento/after.html"/>CSS: Pseudoelemento </a></div>
+    <div><a href="./pseudo-elemento/before.html"/>CSS: Pseudoelemento </a></div>
+    <div><a href="./pseudo-elemento/first-letter.html"/>CSS: Pseudoelemento </a></div>
+    <div><a href="./pseudo-elemento/first-line.html"/>CSS:Pseudoelemento </a></div>
+
+    <div><a href="./flexbox/flexbox.html"/>CSS: Flexbox </a></div> 
+    <div><a href="./flexbox/flexbox_basis.html"/>CSS: Flexbox</a></div>
+    <div><a href="./flexbox/flexbox_align.html"/>CSS: Flexbox Align</a></div>
+    <div><a href="./flexbox/flexbox_flex-direction.html"/>CSS: Flexbox direction</a></div>
+    <div><a href="./flexbox/flexbox_flex-elemento_flex-grow.html"/>CSS: Flexbox flex-grow </a></div>
+    <div><a href="./flexbox/flexbox_flex-elemento_order.html"/>CSS: Flexbox order</a></div>
+    <div><a href="./flexbox/flexbox_flex-justify.html"/>CSS: Flexbox justify</a></div>
+    <div><a href="./flexbox/flexbox_flex-wrap.html"/>CSS: Flexbox wrap </a></div>
+    
+
+    <div><a href="./transiciones/transition.html"/>CSS: Transiciones </a></div>
+    <div><a href="./transiciones/transition_size.html"/>CSS: Transiciones size </a></div>
+    <div><a href="./transiciones/transition_textbox.html"/>CSS: Transiciones texbox</a></div>
+</body>
+</div>
